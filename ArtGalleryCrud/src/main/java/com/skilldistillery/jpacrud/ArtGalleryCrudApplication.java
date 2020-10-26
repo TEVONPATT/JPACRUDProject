1243149e0dd54a5ff4829cf142c536ac41fec49d
Tevon Patterson <Tevonpatt@gmail.com>
2020-10-26 09:13:39 -0700

    WAR deployment configuration

--- ArtGalleryCrud/src/main/java/com/skilldistillery/jpacrud/ArtGalleryCrudApplication.java
+++ ArtGalleryCrud/src/main/java/com/skilldistillery/jpacrud/ArtGalleryCrudApplication.java
@@ -3,10 +3,16 @@ package com.skilldistillery.jpacrud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-//@EntityScan("com.skilldistillery.jpacrud")
-public class ArtGalleryCrudApplication {
+@EntityScan("com.skilldistillery.jpacrud")
+public class ArtGalleryCrudApplication extends SpringBootServletInitializer {
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(ArtGalleryCrudApplication.class);
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ArtGalleryCrudApplication.class, args);
